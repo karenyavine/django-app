4554f9acb6bcf110e8d31010c405a15d161c0978
Karen Yavine <karen@snyk.io>
2018-11-06 10:23:21 +0200

    feat: hello all

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -2,10 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render,get_object_or_404
 from django.template import loader
 from django.http import Http404
+from .models import Names
 
 def index(request):
 
-    name = 'Karen'
+    name = 'all'
     template = loader.get_template('hello/index.html')
     context = {
         'name': name,
@@ -13,4 +14,9 @@ def index(request):
     return render(request, 'hello/index.html', context)
 
 def detail(request, name):
-    return render(request, 'hello/name.html', {'name': 'karen'})
+    names_list = Names.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('hello/name.html')
+    context = {
+        'names_list': names_list,
+    }
+    return render(request, 'hello/name.html', context)
